9d8a3da08eaaefe61d84b5ffbdcbfaf44e93d006
Ahmet Borslan <borslanahmet@gmail.com>
2022-07-25 14:37:38 +0300

    Overflow Menu Component

--- UIKittenComponents/src/App.tsx
+++ UIKittenComponents/src/App.tsx
@@ -18,13 +18,13 @@ import SelectComponent from './Components/Select/SelectComponent';
 import AutocompleteComponent from './Components/Autocomplete/AutocompleteComponent';
 import ModalComponent from './Components/Modal/ModalComponent';
 import TopNavigationComponent from './Components/TopNavigation/TopNavigationComponent';
-import MenuComponent from './Components/Menu/MenuComponent';
+import OverflowMenuComponent from './Components/OverflowMenu/OverflowMenuComponent';
 
 
 const App=() => (
 <ApplicationProvider {...eva} theme={eva.light}>
 <IconRegistry icons={EvaIconsPack} />
-    <MenuComponent/>
+    <OverflowMenuComponent/>
 </ApplicationProvider>
   
 );
