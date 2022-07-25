134a88cc2c8572e2b2b18fcb225f6b32b5d69eef
Ahmet Borslan <borslanahmet@gmail.com>
2022-07-25 11:11:52 +0300

    Icon Component
    Icon Component Usage

--- UIKittenComponents/src/App.tsx
+++ UIKittenComponents/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, Layout, Text,IconRegistry } from '@ui-kitten/components';
 import { StyleSheet } from 'react-native';
 import Layouts from './Layout/layout';
 import TextComponent from './Text/TextComponent';
-
+import IconComponent from './Icon/IconComponent';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 
 const App=() => (
 <ApplicationProvider {...eva} theme={eva.light}>
-    <TextComponent />
+<IconRegistry icons={EvaIconsPack} />
+    <IconComponent />
 </ApplicationProvider>
   
 );
